879e1f9d8f0b2647a0a67223003d122bdb53e5ac
Xiqi Zheng <74235224+xiqi-zheng@users.noreply.github.com>
2024-03-17 15:36:55 +0000

    Add human smell guide

--- Assets/Scripts/Player/Human/SmellGuide.cs
+++ Assets/Scripts/Player/Human/SmellGuide.cs
@@ -1,18 +1,54 @@
-using System.Collections;
-using System.Collections.Generic;
+
+using Photon.Pun;
 using UnityEngine;
 
-public class SmellGuide : MonoBehaviour
+
+public class SmellGuide : MonoBehaviourPun
 {
-    // Start is called before the first frame update
-    void Start()
+    private GameObject _currentSmellEffect;
+
+    private GameObject _leftSmell;
+    private GameObject _rightSmell;
+    private GameObject _bottomSmell;
+
+
+    private GameObject UICamera;
+
+    private void Start()
     {
+        UICamera = GameObject.Find("UICamera");
         
+        _leftSmell = UICamera.transform.Find("LeftSmell").gameObject;
+        _rightSmell = UICamera.transform.Find("RightSmell").gameObject;
+        _bottomSmell = UICamera.transform.Find("BottomSmell").gameObject;
+
+        _currentSmellEffect = UICamera.transform.Find(transform.name).gameObject;
+        
+
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+
+        if (other.CompareTag("Target"))
+        {
+            if (photonView.IsMine && !_leftSmell.activeSelf && !_rightSmell.activeSelf && !_bottomSmell.activeSelf)
+            {
+                _currentSmellEffect.SetActive(true);
+            }
+            
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Target"))
+        {
+            if(photonView.IsMine)
+            {
+                _currentSmellEffect.SetActive(false);
+            }
+        }
+
     }
 }
