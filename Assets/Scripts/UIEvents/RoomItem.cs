df79bb2ac923f8dbffa390adab23e281e6763fda
eclipse <1505703611@qq.com>
2024-04-25 00:45:47 +0100

    fix: use green to show the ready players

--- Assets/Scripts/UIEvents/RoomItem.cs
+++ Assets/Scripts/UIEvents/RoomItem.cs
@@ -3,11 +3,11 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using Photon.Pun;
-using UnityEngine.Serialization;
 using UnityEngine.UI;
+using Photon.Realtime;
 using Hashtable = ExitGames.Client.Photon.Hashtable;
 
-public class RoomItem : MonoBehaviour
+public class RoomItem : MonoBehaviourPunCallbacks
 {
     public int ownerId;
     public bool isReady;
@@ -27,6 +27,7 @@ public class RoomItem : MonoBehaviour
         {
             transform.Find("Button").GetComponent<Button>().onClick.AddListener(OnReadyBtn);
             nameInputField.interactable = true;
+            transform.Find("Button").GetComponent<Image>().color = new Color(0.506f, 0.035f, 0.890f, 1.0f);
         }
         else
         {
@@ -46,18 +47,27 @@ public class RoomItem : MonoBehaviour
 
         PhotonNetwork.LocalPlayer.SetCustomProperties(table);
 
-        ChangeReady(isReady);
-
-        nameInputField.interactable = !isReady;
-
+        Button button = transform.Find("Button").GetComponent<Button>();
+        Image buttonImage = button.GetComponent<Image>();
+        Debug.Log("Current Button Color: " + buttonImage.color);
 
         if (isReady)
         {
+            buttonImage.color = Color.green;
             string newName = nameInputField.text;
             Hashtable props = new Hashtable();
             props.Add("PlayerName", newName);
             PhotonNetwork.LocalPlayer.SetCustomProperties(props);
         }
+        else
+        {
+            buttonImage.color = new Color(0.506f, 0.035f, 0.890f, 1.0f);
+        }
+
+        ChangeReady(isReady);
+
+        nameInputField.interactable = !isReady;
+
     }
 
 
@@ -85,5 +95,29 @@ public class RoomItem : MonoBehaviour
         {
             nameInputField.text = name;
         }
+    }
+
+    public override void OnPlayerPropertiesUpdate(Player targetPlayer, Hashtable changedProps)
+    {
+        if (targetPlayer.ActorNumber == ownerId && changedProps.ContainsKey("IsReady"))
+        {
+            isReady = (bool)changedProps["IsReady"];
+            UpdateButtonColor();
+        }
+    }
+    
+    private void UpdateButtonColor()
+    {
+        Image buttonImage = transform.Find("Button").GetComponent<Image>();
+        if (isReady)
+        {
+            buttonImage.color = Color.green;
+        }
+        else
+        {
+            buttonImage.color = Color.black;
+        }
+
+        ChangeReady(isReady);
     }
 }
